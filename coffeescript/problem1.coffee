9193c5a0784ff58b51e94c87eb66b57674094fde
Cameron Pickham <cameronpickham@gmail.com>
2015-08-27 21:43:54 -0700

    Deleting old files... restarting

--- coffeescript/problem1.coffee
+++
@@ -1,3 +0,0 @@
-sum = 0
-(sum += i if i%3 is 0 or i%5 is 0) for i in [0...1000]
-console.log sum
--- python/problem2.py
+++
@@ -1,7 +0,0 @@
-x, y, total = 1, 0, 0
-while x < 4000000:
-	x, y = x + y, x
-	if x % 2 == 0:
-		total += x
-
-print total
